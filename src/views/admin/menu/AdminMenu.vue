b84dc8c56fd7000164f192d519c27a37ce8d11fd
신유빈 <jumbochips@JumboChips.local>
2024-10-09 15:50:15 +0900

    admin 수정 및 업데이트

--- src/views/admin/menu/AdminMenu.vue
+++ src/views/admin/menu/AdminMenu.vue
@@ -1,15 +1,17 @@
 <template>
-    <Header title="메뉴 관리"></Header>
-    <AdminCalender></AdminCalender>
-
+  <Header title="메뉴 관리"></Header>
+  <AdminCalender></AdminCalender>
+  <AdminFooter />
 </template>
 <script>
-import AdminCalender from '@/components/admin/calender/MenuCalender.vue'
+import AdminCalender from "@/components/admin/calender/MenuCalender.vue";
+import AdminFooter from "@/components/admin/layout/AdminFooter.vue";
 
 export default {
-    components:{
-        AdminCalender: AdminCalender,
-    }
-}
+  components: {
+    AdminCalender: AdminCalender,
+    AdminFooter,
+  },
+};
 </script>
 <style></style>
