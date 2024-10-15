cb43b011d671b0cfa69a293397fa7e164e74fd85
codeSage <ots1120@gmail.com>
2024-10-15 13:15:15 +0900

    css, tailwind 적용 문제 해결

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue';
 import store from '@/store/index.js';
 import router from '@/router/index.js';
 
+// tailwind, css styles
+import '@/assets/styles/reset.css';
+import '@/assets/styles/font.css';
+import '@/assets/styles/styles.css';
+
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
 console.log(router);
+
 app.mount('#app');
